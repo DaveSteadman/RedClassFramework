f840a683711cf946864b781111637d76b4a47be8
davidgsteadman <davidgsteadman@yahoo.co.uk>
2016-01-24 21:00:00 +0000

    Copyright statements.

--- Geometry/RedCirclularArc.h
+++ Geometry/RedCirclularArc.h
@@ -1,3 +1,21 @@
+// -------------------------------------------------------------------------------------------------
+// This file is covered by: The MIT License (MIT) Copyright (c) 2016 David G. Steadman
+// -------------------------------------------------------------------------------------------------
+// Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
+// associated documentation files (the "Software"), to deal in the Software without restriction,
+// including without limitation the rights to use, copy, modify, merge, publish, distribute,
+// sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT
+// NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+// DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+// -------------------------------------------------------------------------------------------------
+// (http://opensource.org/licenses/MIT)
+// -------------------------------------------------------------------------------------------------
+
 #pragma once
  
 #include "RedPoint2D.h"
@@ -8,36 +26,37 @@ namespace Geometry {
 
 using namespace Red::Core;
 
-       /// Arc class exists to define circular arc maths. This in turn supports the definition of a rounded rectangle.
-       class RedCircularArc
-       {
-       public:
- 
-              // Constructors
-              RedCircularArc (const RedPoint2D& p, const RedNumber r) : origin(p), radius(r) {};
-              RedCircularArc (const RedCircle& c) : origin(c.origin), radius (c.radius) {};
- 
-              // Simple Assigns / Queries
- 
-              // create from rect, points, three points
-              // Get points on circumference (for maths or drawing)
-             
-              // Bounding box
-             
-              // point-in-circle: get vector of distance
-              RedVector2D VectorOffsetFromOrigin(const RedPoint2D<RedNumber>& p);
-              
-                const RedNumber DistanceFromOriginToPoint(const RedPoint2DUnit p) const;
+/// Arc class exists to define circular arc maths. This in turn supports the definition of a rounded rectangle.
+class RedCircularArc
+{
+public:
 
-             /// \todo Point routine to contribute to drawing
-             const RedPoint2DUnit PointAtAngle(RedNumber a);
- 
-       private:
-              RedPoint2D             origin;
-              RedRectPositions     rectPos;
-              RedNumber            startAngle;
-              RedNumber            endAngle;
- 
-       };
+      // Constructors
+      RedCircularArc (const RedPoint2D& p, const RedNumber r) : origin(p), radius(r) {};
+      RedCircularArc (const RedCircle& c) : origin(c.origin), radius (c.radius) {};
+
+      // Simple Assigns / Queries
+
+      // create from rect, points, three points
+      // Get points on circumference (for maths or drawing)
+     
+      // Bounding box
+     
+      // point-in-circle: get vector of distance
+      RedVector2D VectorOffsetFromOrigin(const RedPoint2D<RedNumber>& p);
+      
+        const RedNumber DistanceFromOriginToPoint(const RedPoint2DUnit p) const;
+
+     /// \todo Point routine to contribute to drawing
+     const RedPoint2DUnit PointAtAngle(RedNumber a);
+
+private:
+      RedPoint2D             origin;
+      RedRectPositions     rectPos;
+      RedNumber            startAngle;
+      RedNumber            endAngle;
+
+};
+
 }
 }
--- Geometry/RedDistance.h
+++ Geometry/RedDistance.h
@@ -1,3 +1,20 @@
+// -------------------------------------------------------------------------------------------------
+// This file is covered by: The MIT License (MIT) Copyright (c) 2016 David G. Steadman
+// -------------------------------------------------------------------------------------------------
+// Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
+// associated documentation files (the "Software"), to deal in the Software without restriction,
+// including without limitation the rights to use, copy, modify, merge, publish, distribute,
+// sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT
+// NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+// DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+// -------------------------------------------------------------------------------------------------
+// (http://opensource.org/licenses/MIT)
+// -------------------------------------------------------------------------------------------------
 
 #pragma once
 
@@ -7,36 +24,36 @@
 namespace Red {
 namespace Units {
 
-    using namespace Red::Core;
+using namespace Red::Core;
 
-    class RedDistance : public RedNumber
-    {
-    public:
+class RedDistance : public RedNumber
+{
+public:
 
-        RedDistance(void)                            { Init(); };
-        RedDistance(const RedNumber& val)            { Set(val); };
-        RedDistance(const RedDistance& dist)         { Set(dist.Metres()); };
+    RedDistance(void)                            { Init(); };
+    RedDistance(const RedNumber& val)            { Set(val); };
+    RedDistance(const RedDistance& dist)         { Set(dist.Metres()); };
 
-        // Metric
-        void SetKilometres(const RedNumber& newval)  { Set(newval * metresPerKilometre); };
-        void SetMetres(const RedNumber& newval)      { Set(newval); };
-        void SetCentimetres(const RedNumber& newval) { Set(newval * metresPerCentimetre); };
-        void SetMillimetres(const RedNumber& newval) { Set(newval * metresPerMillimetre); };
-        RedNumber Kilometres(void) const             { RedNumber retval(*this); retval *= kilometresPerMetre; return retval; };
-        RedNumber Metres(void) const                 { RedNumber retval(*this); return retval; };
-        RedNumber Centimetres(void) const            { RedNumber retval(*this); retval *= centimetresPerMetre; return retval; };
-        RedNumber Millimetres(void) const            { RedNumber retval(*this); retval *= millimetresPerMetre; return retval; };
+    // Metric
+    void SetKilometres(const RedNumber& newval)  { Set(newval * metresPerKilometre); };
+    void SetMetres(const RedNumber& newval)      { Set(newval); };
+    void SetCentimetres(const RedNumber& newval) { Set(newval * metresPerCentimetre); };
+    void SetMillimetres(const RedNumber& newval) { Set(newval * metresPerMillimetre); };
+    RedNumber Kilometres(void) const             { RedNumber retval(*this); retval *= kilometresPerMetre; return retval; };
+    RedNumber Metres(void) const                 { RedNumber retval(*this); return retval; };
+    RedNumber Centimetres(void) const            { RedNumber retval(*this); retval *= centimetresPerMetre; return retval; };
+    RedNumber Millimetres(void) const            { RedNumber retval(*this); retval *= millimetresPerMetre; return retval; };
 
-        // Imperial
-        void SetMiles(const RedNumber& newval)       { Set(newval * metresPerMile); };
-        void SetYards(const RedNumber& newval)       { Set(newval * metresPerYard); };
-        void SetFeet(const RedNumber& newval)        { Set(newval * metresPerFoot); };
-        void SetInches(const RedNumber& newval)      { Set(newval * metresPerInch); };
-        RedNumber Miles(void) const                  { RedNumber retval(*this); retval *= milesPerMetre;  return retval; };
-        RedNumber Yards(void) const                  { RedNumber retval(*this); retval *= yardsPerMetre;  return retval; };
-        RedNumber Feet(void) const                   { RedNumber retval(*this); retval *= feetPerMetre;   return retval; };
-        RedNumber Inches(void) const                 { RedNumber retval(*this); retval *= inchesPerMetre; return retval; };
-    };
+    // Imperial
+    void SetMiles(const RedNumber& newval)       { Set(newval * metresPerMile); };
+    void SetYards(const RedNumber& newval)       { Set(newval * metresPerYard); };
+    void SetFeet(const RedNumber& newval)        { Set(newval * metresPerFoot); };
+    void SetInches(const RedNumber& newval)      { Set(newval * metresPerInch); };
+    RedNumber Miles(void) const                  { RedNumber retval(*this); retval *= milesPerMetre;  return retval; };
+    RedNumber Yards(void) const                  { RedNumber retval(*this); retval *= yardsPerMetre;  return retval; };
+    RedNumber Feet(void) const                   { RedNumber retval(*this); retval *= feetPerMetre;   return retval; };
+    RedNumber Inches(void) const                 { RedNumber retval(*this); retval *= inchesPerMetre; return retval; };
+};
 
 } // Units
 } // Red
