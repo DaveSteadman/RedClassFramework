e8675dd785b5baff7f526f0d817cd6ed21b30932
davidgsteadman <davidgsteadman@yahoo.co.uk>
2016-01-25 13:15:39 +0000

    Update RedArea.h

--- Geometry/RedArea.h
+++ Geometry/RedArea.h
@@ -18,24 +18,24 @@
 
 #pragma once
 
-#include "RedCoreNamespace.h"
+#include "RedNumber.h"
 #include "ConversionConstants.h"
 
 namespace Red {
 namespace Geometry {
 
-    using namespace Red::Core;
+using namespace Red::Core;
 
-    class RedArea : public RedNumber
-    {
-    public:
-        void SetSquareMetres(const RedNumber& newval) { Set(newval); };
-        void SetAcres(const RedNumber& newval)        { Set(newval * squaremetresPerAcre); };
+class RedArea : public RedNumber
+{
+public:
+    void SetSquareMetres(const RedNumber& newval) { Set(newval); };
+    void SetAcres(const RedNumber& newval)        { Set(newval * squaremetresPerAcre); };
 
-        RedNumber SquareMetres(void) const            { RedNumber retval(*this); return retval; };
-        RedNumber Acres(void) const                   { RedNumber retval(*this); retval *= acresPerSquaremetre; return retval; };
+    RedNumber SquareMetres(void) const            { RedNumber retval(*this); return retval; };
+    RedNumber Acres(void) const                   { RedNumber retval(*this); retval *= acresPerSquaremetre; return retval; };
 
-    };
+};
 
 } // Geometry
 } // Red
