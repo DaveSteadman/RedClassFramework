b5eb5279893e8d7e935f1cfad8820014648eed74
davidgsteadman <davidgsteadman@yahoo.co.uk>
2016-01-25 13:16:47 +0000

    Update RedSphere.h

--- Geometry/RedSphere.h
+++ Geometry/RedSphere.h
@@ -18,37 +18,34 @@
 
 #pragma once
 
-#include "RedCoreNamespace.h"
-#include "RedUnitsNamespace.h"
-
+#include "RedNumber.h"
 #include "RedPoint3D.h"
-#include "RedLatLong.h"
 
 namespace Red {
 namespace Geometry {
 
 using namespace Red::Core;
 
-    class RedSphere
-    {
-    public:
-     
-        // Constructors
-        RedSphere(void) { Init(); };
-        RedSphere(const RedPoint3D& no, const RedNumber& nr) { Set(no, nr); };
+class RedSphere
+{
+public:
+ 
+    // Constructors
+    RedSphere(void) { Init(); };
+    RedSphere(const RedPoint3D& no, const RedNumber& nr) { Set(no, nr); };
 
-        // Basic accessors
-        void Init(void) { origin.Init(); radius=0; };
-        void Set(const RedPoint3D& no, const RedNumber& nr) { origin=no; radius=nr; };
+    // Basic accessors
+    void Init(void) { origin.Init(); radius=0; };
+    void Set(const RedPoint3D& no, const RedNumber& nr) { origin=no; radius=nr; };
 
-        RedNumber SurfaceArea(void)   const { return radius*radius * 4.0 * Red::Units::pi; }; // =4.pi.r^2
-        RedNumber Volume(void)        const { return radius*radius*radius * (4.0 / 3.0) * Red::Units::pi; };  // =4/3.pi.r^3
-        RedNumber Circumference(void) const { return radius * 2 * Red::Units::pi; }; // =2.pi.r
+    RedNumber SurfaceArea(void)   const { return radius*radius * 4.0 * Red::Units::pi; }; // =4.pi.r^2
+    RedNumber Volume(void)        const { return radius*radius*radius * (4.0 / 3.0) * Red::Units::pi; };  // =4/3.pi.r^3
+    RedNumber Circumference(void) const { return radius * 2 * Red::Units::pi; }; // =2.pi.r
 
-    private:
-        RedPoint3D  origin;
-        RedNumber   radius;
-    };
+private:
+    RedPoint3D  origin;
+    RedNumber   radius;
+};
 
 } // Geometry
 } // Red
