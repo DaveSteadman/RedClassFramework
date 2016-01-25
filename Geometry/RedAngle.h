3965360bb38f89d83a0a8a6373f1cafc96ef140a
davidgsteadman <davidgsteadman@yahoo.co.uk>
2016-01-25 13:08:49 +0000

    Update RedAngle.h

--- Geometry/RedAngle.h
+++ Geometry/RedAngle.h
@@ -18,25 +18,25 @@
 
 #pragma once
 
-#include "RedCoreNamespace.h"
+#include "RedNumber.h"
 #include "ConversionConstants.h"
 
 namespace Red {
 namespace Geometry {
 
-    using namespace Red::Core;
+using namespace Red::Core;
 
-    static const RedNumberRange kDegreesFullCircle  = RedNumberRange(0, 360,      kRedNumberRangeWrapOnUpper); // wrap, so exact 360 isn't possible
-    static const RedNumberRange kRadiansFullCircle  = RedNumberRange(0, 2.0 * pi, kRedNumberRangeWrapOnUpper); // wrap, so exact 360 isn't possible
+static const RedNumberRange kDegreesFullCircle  = RedNumberRange(0, 360,      kRedNumberRangeWrapOnUpper); // wrap, so exact 360 isn't possible
+static const RedNumberRange kRadiansFullCircle  = RedNumberRange(0, 2.0 * pi, kRedNumberRangeWrapOnUpper); // wrap, so exact 360 isn't possible
 
-    class RedAngle : public RedNumber
-    {
-    public:
-        void      SetRadians(const RedNumber& newval) { Set(newval); };
-        void      SetDegrees(const RedNumber& newval) { Set(newval * radiansPerDegree); };
-        RedNumber Radians(void) const                 { RedNumber retval(*this); return retval; };
-        RedNumber Degrees(void) const                 { RedNumber retval(*this); retval *= degreesPerRadian; return retval; };
-    };
+class RedAngle : public RedNumber
+{
+public:
+    void      SetRadians(const RedNumber& newval) { Set(newval); };
+    void      SetDegrees(const RedNumber& newval) { Set(newval * radiansPerDegree); };
+    RedNumber Radians(void) const                 { RedNumber retval(*this); return retval; };
+    RedNumber Degrees(void) const                 { RedNumber retval(*this); retval *= degreesPerRadian; return retval; };
+};
 
 } // Geometry
 } // Red
