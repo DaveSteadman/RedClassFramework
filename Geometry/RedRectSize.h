255e0140b6030a564918b4df02118c3ed0d94652
davidgsteadman <davidgsteadman@yahoo.co.uk>
2016-01-25 13:11:04 +0000

    Update RedRectSize.h

--- Geometry/RedRectSize.h
+++ Geometry/RedRectSize.h
@@ -18,35 +18,35 @@
 
 #pragma once
 
-#include "RedCoreNamespace.h"
+#include "RedNumber.h"
 
 namespace Red {
 namespace Geometry {
 
 using namespace Red::Core;
 
-    class RedRectSize
-    {
-    public:
-        RedRectSize() { Init(); };
+class RedRectSize
+{
+public:
+    RedRectSize() { Init(); };
 
-        void Init(void)                    { width=0; height=0; };
-        void Set(RedRectSize newsize)      { width=newsize.width; height=newsize.height; };
-        void Set(const RedNumber& w, const RedNumber& h) { width=w; height=h; };
+    void Init(void)                                  { width=0; height=0; };
+    void Set(const RedRectSize& newsize)             { width=newsize.width; height=newsize.height; };
+    void Set(const RedNumber& w, const RedNumber& h) { width=w; height=h; };
 
-        void Adjust(const RedNumber& dx, const RedNumber& dy) { width+=dx; height+=dy; };
+    void Adjust(const RedNumber& dx, const RedNumber& dy) { width+=dx; height+=dy; };
 
-        void Scale(const RedNumber& f)     { width*=f; height*=f; };
+    void Scale(const RedNumber& f)     { width*=f; height*=f; };
 
-        const RedNumber Width(void)  const { return width;  };
-        const RedNumber Height(void) const { return height; };
+    const RedNumber Width(void)  const { return width;  };
+    const RedNumber Height(void) const { return height; };
 
-        void operator =(const RedRectSize& newsize) { Set(newsize); };
-        
-    private: 
-        RedNumber width;
-        RedNumber height;
-    };
+    void operator =(const RedRectSize& newsize) { Set(newsize); };
+    
+private: 
+    RedNumber width;
+    RedNumber height;
+};
 
 } // Geometry
 } // Red
