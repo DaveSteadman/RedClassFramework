86445d4796201ef9bc764191dfe04011f7f9ceac
davidgsteadman <davidgsteadman@yahoo.co.uk>
2016-01-25 13:09:11 +0000

    Update RedVolume.h

--- Geometry/RedVolume.h
+++ Geometry/RedVolume.h
@@ -18,28 +18,28 @@
 
 #pragma once
 
-#include "RedCoreNamespace.h"
+#include "RedNumber.h"
 #include "ConversionConstants.h"
 
 namespace Red {
 namespace Geometry {
 
-    using namespace Red::Core;
+using namespace Red::Core;
 
-    class RedVolume : public RedNumber
-    {
-    public:
+class RedVolume : public RedNumber
+{
+public:
 
-        // Metric
-        void SetLitres(const RedNumber& newval)  { Set(newval); };
-        RedNumber GetLitres(void) const          { RedNumber retval(*this); return retval; };
+    // Metric
+    void SetLitres(const RedNumber& newval)  { Set(newval); };
+    RedNumber GetLitres(void) const          { RedNumber retval(*this); return retval; };
 
-        // Imperial
-        void SetPints(const RedNumber& newval)   { Set(newval * pintsPerLitre); };
-        void SetGallons(const RedNumber& newval) { Set(newval * gallonsPerLitre); };
-        RedNumber Pints(void) const              { RedNumber retval(*this); retval *= pintsPerLitre;   return retval; };
-        RedNumber Gallons(void) const            { RedNumber retval(*this); retval += gallonsPerLitre; return retval; };
-    };
+    // Imperial
+    void SetPints(const RedNumber& newval)   { Set(newval * pintsPerLitre); };
+    void SetGallons(const RedNumber& newval) { Set(newval * gallonsPerLitre); };
+    RedNumber Pints(void) const              { RedNumber retval(*this); retval *= pintsPerLitre;   return retval; };
+    RedNumber Gallons(void) const            { RedNumber retval(*this); retval += gallonsPerLitre; return retval; };
+};
 
 } // Geometry
 } // Red
