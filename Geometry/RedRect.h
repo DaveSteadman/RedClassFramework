1d8755c8d579d46cd029ec47d77fa60b831b96fd
davidgsteadman <davidgsteadman@yahoo.co.uk>
2016-01-25 13:10:07 +0000

    Update RedRect.h

--- Geometry/RedRect.h
+++ Geometry/RedRect.h
@@ -28,35 +28,35 @@ namespace Geometry {
 
 using namespace Red::Core;
 
-    class RedRect
-    {
-    public:
-        RedRect() { Init(); };
-        RedRect(const RedRect& nr) { Set(nr); };
-        RedRect(const RedPoint2D& no, const RedRectSize& ns) { Set(no, ns); };
-        RedRect(const RedNumber& nx1, const RedNumber& ny1, const RedNumber& nx2, const RedNumber& ny2) { Set(nx1, ny1, nx2, ny2); };
+class RedRect
+{
+public:
+    RedRect() { Init(); };
+    RedRect(const RedRect& nr) { Set(nr); };
+    RedRect(const RedPoint2D& no, const RedRectSize& ns) { Set(no, ns); };
+    RedRect(const RedNumber& nx1, const RedNumber& ny1, const RedNumber& nx2, const RedNumber& ny2) { Set(nx1, ny1, nx2, ny2); };
 
-        void Init(void)            { origin.Init(); size.Init(); };
+    void Init(void)            { origin.Init(); size.Init(); };
 
-        void Set(const RedRect& nr) { origin=nr.origin; size=nr.size; };
-        void Set(const RedPoint2D& no, const RedRectSize& ns) { origin=no; size=ns; };
-        void Set(const RedNumber& nx1, const RedNumber& ny1, const RedNumber& nx2, const RedNumber& ny2) { origin.Set(nx1,ny1); size.Set(nx2-nx1, ny2-ny1); };
+    void Set(const RedRect& nr) { origin=nr.origin; size=nr.size; };
+    void Set(const RedPoint2D& no, const RedRectSize& ns) { origin=no; size=ns; };
+    void Set(const RedNumber& nx1, const RedNumber& ny1, const RedNumber& nx2, const RedNumber& ny2) { origin.Set(nx1,ny1); size.Set(nx2-nx1, ny2-ny1); };
 
-        void              SetOrigin(const RedPoint2D& no)  { origin=no; };
-        void              SetSize(const RedRectSize& ns) { size=ns; };
-        const RedPoint2D    Origin(void) const { return origin; };
-        const RedRectSize Size(void)   const { return size; };
+    void              SetOrigin(const RedPoint2D& no){ origin=no; };
+    void              SetSize(const RedRectSize& ns) { size=ns; };
+    const RedPoint2D  Origin(void) const { return origin; };
+    const RedRectSize Size(void)   const { return size; };
 
-        const int IsPointInRect(RedPoint2D p) const;
+    const int IsPointInRect(RedPoint2D p) const;
 
-        void Inset(const RedNumber& i);
+    void Inset(const RedNumber& i);
 
-        void operator =(const RedRect& newvect) { Set(newvect); };
+    void operator =(const RedRect& newvect) { Set(newvect); };
 
-    private:
-        RedPoint2D    origin;
-        RedRectSize size; 
-    };
+private:
+    RedPoint2D  origin;
+    RedRectSize size; 
+};
 
 } // Geometry
 } // Red
