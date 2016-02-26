285e3cdc7602f3aa85b0b4b735bc2b5bf3e3a41a
davidgsteadman <davidgsteadman@yahoo.co.uk>
2016-02-26 12:53:55 +0000

    Update RedTime.h

--- Core/RedTime.h
+++ Core/RedTime.h
@@ -48,27 +48,27 @@ public:
     void Init(void) { hour=0; minute=0; seconds=0; };
 
     void SetTime(const unsigned h, const unsigned m, const double& s)        { hour=h; minute=m; seconds=s; };
-	void SetTime(const RedNumber& h, const RedNumber& m, const RedNumber& s) { hour=h.IntegerValue(); minute=m.IntegerValue(); seconds=s.DoubleValue(); };
-	void SetTime(const RedString& timestr);
+    void SetTime(const RedNumber& h, const RedNumber& m, const RedNumber& s) { hour=h.IntegerValue(); minute=m.IntegerValue(); seconds=s.DoubleValue(); };
+    void SetTime(const RedString& timestr);
 
-	void SetNow(void);
+    void SetNow(void);
 
-	// Time Presentation
+    // Time Presentation
     const RedString TimeString(void) const;
     const int       SixDigitInt(void) const;
 
 	// Simple Access Routines
     const RedNumber Hours     (void) const                  { return hour;    };
-	const RedNumber Minutes   (void) const                  { return minute;  };
-	const RedNumber Seconds   (void) const                  { return seconds; };
-	void            SetHours  (const RedNumber& NewHours)   { hour    = NewHours;   };
-	void            SetMinutes(const RedNumber& NewMinutes) { minute  = NewMinutes; };
-	void            SetSeconds(const RedNumber& NewSeconds) { seconds = NewSeconds; };
+    const RedNumber Minutes   (void) const                  { return minute;  };
+    const RedNumber Seconds   (void) const                  { return seconds; };
+    void            SetHours  (const RedNumber& NewHours)   { hour    = NewHours;   };
+    void            SetMinutes(const RedNumber& NewMinutes) { minute  = NewMinutes; };
+    void            SetSeconds(const RedNumber& NewSeconds) { seconds = NewSeconds; };
 
-	// Seconds In Day Routines
-	const RedNumber SecondsElapsedInDay(void) const { return (hour*kTimeSecondsInHour) + (minute*kTimeSecondsInMinute) + seconds; };
-	const RedNumber SecondsLeftInDay(void)    const { return kTimeSecondsInDay - SecondsElapsedInDay(); };
-	void            SetTimeFromElapsedSeconds(const RedNumber& DayElapsedSeconds);
+    // Seconds In Day Routines
+    const RedNumber SecondsElapsedInDay(void) const { return (hour*kTimeSecondsInHour) + (minute*kTimeSecondsInMinute) + seconds; };
+    const RedNumber SecondsLeftInDay(void)    const { return kTimeSecondsInDay - SecondsElapsedInDay(); };
+    void            SetTimeFromElapsedSeconds(const RedNumber& DayElapsedSeconds);
 
 private:
 
