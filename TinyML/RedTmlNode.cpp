9b48d2693e1c131709e6b91070038a97ecc97227
Dave Steadman <davesteadman@me.com>
2022-02-18 12:40:45 +0000

    Update RedTmlNode.cpp

--- TinyML/RedTmlNode.cpp
+++ TinyML/RedTmlNode.cpp
@@ -29,23 +29,23 @@ namespace TinyML {
 
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
-// RedTmlNode::~RedTmlNode()
-// {
-//     RedTmlNode* pCurrNode = 0;
+RedTmlNode::~RedTmlNode()
+{
+    RedTmlNode* pCurrNode = 0;
     
-//     // Loop until the list is empty
-//     while (!this->nodelist.IsEmpty())
-//     {
-//         // If we find the last node in the list alright
-//         if (this->nodelist.FindLast(&pCurrNode))
-//         {
-//             // if the node contents is not NULL, delete it, then delete the linked list node itself.
-//             if (pCurrNode != NULL)
-//                  delete pCurrNode;
-//             this->nodelist.DelLast();             
-//         }
-//     }
-// }
+    // Loop until the list is empty
+    while (!this->nodelist.IsEmpty())
+    {
+        // If we find the last node in the list alright
+        if (this->nodelist.FindLast(&pCurrNode))
+        {
+            // if the node contents is not NULL, delete it, then delete the linked list node itself.
+            // if (pCurrNode != NULL)
+            //      delete pCurrNode;
+            this->nodelist.DelLast();
+        }
+    }
+}
     
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
